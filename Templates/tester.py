e8730b8cfd28034cce33dcaf54deeee2b739988a
Noel Kocheril <NoelKocheril@Gmail.com>
2021-09-04 20:12:55 -0400

    Added Templates for Test and Problem Python Files

---
+++ Templates/tester.py
@@ -0,0 +1,21 @@
+import unittest
+import problem
+
+
+class TestSumOfTwo(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual()
+
+    def test_2(self):
+        self.assertEqual()
+
+    def test_3(self):
+        self.assertEqual()
+
+    def test_4(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
